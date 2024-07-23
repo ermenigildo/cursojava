a512fdad7841e7ae01423d274336147a2c173849
ermenigildo <47076286@certus.edu.pe>
2024-07-23 18:56:41 -0500

    se agrega algunas condiciones

--- OneDrive/Documentos/NetBeansProjects/datosPorTeclado/src/main/java/com/mycompany/datosporteclado/DatosPorTeclado.java
+++ OneDrive/Documentos/NetBeansProjects/datosPorTeclado/src/main/java/com/mycompany/datosporteclado/DatosPorTeclado.java
@@ -19,22 +19,42 @@ public class DatosPorTeclado {
     public static void main(String[] args) {
         //programa para calcular promedio de notas
         Scanner scanner =new Scanner(System.in);
+        
+        //primera nota
         System.out.print("Ingres la primera nota :");
         double nota1 = scanner.nextDouble();
+        if(nota1>=1&&nota1<=20){
+            System.out.println("Genial ingreso correcto la nota de :"+ nota1);
+        }else{
+            System.out.println("ingrese un numero de 1 al 20 :"+nota1+" no es valido");
+        }
         
+         //segunda  nota
         System.out.print("Ingres la segunda nota :");
         double nota2 = scanner.nextDouble();
+        if(nota2>=1 && nota2<=20){
+            System.out.println("Genial ingreso correcto la nota de :"+ nota2);
+        }else{
+            System.out.println("ingrese un numero de 1 al 20 :"+nota2+" no es valido");
+        }
         
+         //tercera  nota         
         System.out.print("Ingres la tercera nota :");
         double nota3 = scanner.nextDouble();
         if(nota3>=1 && nota3<=20){
-            System.out.println("genial ingresaste la tercera nota");
+            System.out.println("genial ingresaste correcto la nota de "+nota3);
         }else{
-            System.out.println("ingrese un numero de 1 al 20");
+            System.out.println("ingrese un numero de 1 al 20 :"+nota3+" no es valido");
         }
         
+         //cuarta nota
         System.out.print("Ingres la cuarta nota :");
         double nota4 = scanner.nextDouble();
+        if(nota4>=1 && nota4<=20){
+            System.out.println("genial ingresaste correcto la nota de "+nota4);
+        }else{
+            System.out.println("ingrese un numero de 1 al 20 :"+nota4+" no es valido");
+        }
         
         double promedio = (nota1+nota2+nota3+nota4)/4;
         
